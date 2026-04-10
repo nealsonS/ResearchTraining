ec8b8cb685c87030b9dfc11ed6e20c9bb3a500d6
Nealson <nealsonsetiawan03@gmail.com>
2026-04-10 00:49:01 -0700

    added export socks5h server to tailscale_setup

--- training/tailscale_runpod_setup.sh
+++ training/tailscale_runpod_setup.sh
@@ -4,3 +4,5 @@ curl -fsSL https://tailscale.com/install.sh | sh
 
 tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 tailscale up --auth-key=${TAILSCALE_AUTH_KEY}
+
+export ALL_PROXY=socks5h://127.0.0.1:1055
